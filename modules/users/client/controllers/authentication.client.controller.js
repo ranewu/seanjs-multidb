a83be7b8ad6a034befcef8b32f2ea5d8313ec69b
Rane Wu <rane.hr.wu@gmail.com>
2017-11-15 19:27:39 -0800

    updated front end dependencies, not working yet

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -22,13 +22,13 @@ angular.module('user').controller('AuthenticationController', ['$scope', '$state
         return false;
       }
 
-      $http.post('/api/auth/signup', $scope.credentials).success(function(response) {
+      $http.post('/api/auth/signup', $scope.credentials).then(function(response) {
         // If successful we assign the response to the global user model
         $scope.authentication.user = response;
 
         // And redirect to the previous or home page
         $state.go($state.previous.state.name || 'home', $state.previous.params);
-      }).error(function(response) {
+      }).catch(function(response) {
         $scope.error = response.message;
       });
     };
@@ -42,13 +42,13 @@ angular.module('user').controller('AuthenticationController', ['$scope', '$state
         return false;
       }
 
-      $http.post('/api/auth/signin', $scope.credentials).success(function(response) {
+      $http.post('/api/auth/signin', $scope.credentials).then(function(response) {
         // If successful we assign the response to the global user model
         $scope.authentication.user = response;
 
         // And redirect to the previous or home page
         $state.go($state.previous.state.name || 'home', $state.previous.params);
-      }).error(function(response) {
+      }).catch(function(response) {
         $scope.error = response.message;
       });
     };
